4bb8c8b6a4f2821c320423c2a779953e43e70a70
Heyuan Zeng <hi@universe.observer>
2024-03-09 18:01:03 +0000

    fix: serializer fields

--- wherespace-backend/wherespace/restful/serializers/event.py
+++ wherespace-backend/wherespace/restful/serializers/event.py
@@ -1,10 +1,14 @@
 from django.apps import apps
 from rest_framework import serializers
 
+from .space import BookingSerializer
+
 Event = apps.get_model("backend", "Event")
 
 
 class EventSerializer(serializers.ModelSerializer):
+    booking = BookingSerializer(read_only=True)
+
     class Meta:
         model = Event
-        fields = ["id", "space"]
+        fields = ["id", "name", "booking", "start_time", "end_time", "max_attendees"]
--- wherespace-backend/wherespace/restful/serializers/space.py
+++ wherespace-backend/wherespace/restful/serializers/space.py
@@ -1,18 +1,38 @@
 from django.apps import apps
 from rest_framework import serializers
 
+from .user import UserSerializer
+
+Rating = apps.get_model("backend", "Rating")
 Space = apps.get_model("backend", "Space")
 Booking = apps.get_model("backend", "Booking")
 
 
+class RatingSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Rating
+        fields = ["id", "user", "rating"]
+        read_only_fields = ["id"]
+
+
 class SpaceSerializer(serializers.ModelSerializer):
+    ratings = RatingSerializer(
+        many=True, read_only=True
+    )  # TODO: should it be read only?
+
     class Meta:
         model = Space
-        fields = ["id", "name", "google_map_url"]
+        fields = ["id", "name", "google_map_url", "image_url", "description", "ratings"]
         read_only_fields = ["id"]
 
 
 class BookingSerializer(serializers.ModelSerializer):
+    space = SpaceSerializer()
+    attendees = UserSerializer(many=True)
+    host = UserSerializer(read_only=True)
+
     class Meta:
         model = Booking
         fields = ["id", "space", "start_time", "end_time", "host", "attendees"]
