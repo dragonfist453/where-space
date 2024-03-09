a1e61ec1fd95b9f2956405ce64443154b8b5f797
FriedhelmWS <352063465@qq.com>
2024-03-09 23:39:23 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "medium-champagne": "#F6EEAB",
+        "spring-bud": "#C9DD94",
+        "eton-blue": "#9DCF94",
+        "dark-sea-green": "#7EC796",
+        "ocean-green": "#5EBD96",
+        "persian-green": "#11A797",
+      },
     },
   },
   plugins: [],
