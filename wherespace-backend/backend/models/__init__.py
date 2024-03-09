a55e5b67fa574eeeb6972b0b11816eb35ef25113
Heyuan Zeng <hi@universe.observer>
2024-03-09 20:20:41 +0000

    feat: add chat rooms

--- wherespace-backend/backend/models/__init__.py
+++ wherespace-backend/backend/models/__init__.py
@@ -1,5 +1,16 @@
+from .chat_room import EventRoom, PrivateChat, PrivateMessage, EventMessage
 from .event import Event
 from .space import Booking, Space
 from .user import UserPrivacy, User
 
-__all__ = ["Event", "Booking", "Space", "UserPrivacy", "User"]
+__all__ = [
+    "Event",
+    "Booking",
+    "Space",
+    "UserPrivacy",
+    "User",
+    "EventRoom",
+    "PrivateMessage",
+    "PrivateChat",
+    "EventMessage",
+]
---
+++ wherespace-backend/backend/models/chat_room.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+from .mixins import UUIDMixin
+
+
+class _Message(UUIDMixin, models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["created_at"]
+
+
+class EventMessage(_Message):
+    event = models.ForeignKey(
+        "EventRoom",
+        on_delete=models.CASCADE,
+        related_name="event_messages",
+        related_query_name="event_message",
+    )
+    sender = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="event_messages",
+        related_query_name="event_message",
+    )
+
+
+class EventRoom(UUIDMixin, models.Model):
+    event = models.OneToOneField(
+        "Event",
+        on_delete=models.CASCADE,
+        related_name="chat_room",
+        related_query_name="chat_room",
+    )
+
+
+class PrivateMessage(_Message):
+    chat = models.ForeignKey(
+        "PrivateChat",
+        on_delete=models.CASCADE,
+        related_name="private_messages",
+        related_query_name="private_message",
+    )
+
+    sender = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="private_messages",
+        related_query_name="private_message",
+    )
+
+
+class PrivateChat(UUIDMixin, models.Model):
+    from_user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="from_private_chats",
+        related_query_name="private_chats",
+    )
+    to_user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="to_private_chats",
+        related_query_name="private_chats",
+    )
