e507cfd02ae011be22da6048fb17fd1ad4c72222
Heyuan Zeng <hi@universe.observer>
2024-03-09 16:37:35 +0000

    fix: user creation

--- wherespace-backend/backend/models/__init__.py
+++ wherespace-backend/backend/models/__init__.py
@@ -1,5 +1,5 @@
 from .event import Event
 from .space import Booking, Space
-from .user import User, UserPrivacy
+from .user import UserPrivacy, User
 
-__all__ = ["Event", "Booking", "Space", "User", "UserPrivacy"]
+__all__ = ["Event", "Booking", "Space", "UserPrivacy", "User"]
--- wherespace-backend/backend/models/user.py
+++ wherespace-backend/backend/models/user.py
@@ -1,22 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .mixins import UUIDMixin
 
 
 class UserPrivacy(UUIDMixin, models.Model):
     is_looking_for_peers = models.BooleanField(default=False)
+    user = models.OneToOneField(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="privacy",
+        related_query_name="privacy",
+    )
 
-    @staticmethod
-    def default_privacy():
-        return UserPrivacy.objects.create(is_looking_for_peers=False).id
+
+def default_privacy():
+    return UserPrivacy.objects.create(is_looking_for_peers=False).id
 
 
 class User(UUIDMixin, AbstractUser):
-    # TODO: privacy default
-    privacy = models.OneToOneField(
-        UserPrivacy,
-        on_delete=models.CASCADE,
-        related_name="user",
-        default=UserPrivacy.default_privacy,
-    )
+    pass
+
+
+@receiver(post_save, sender=User)
+def create_user_privacy(sender, instance, created, **kwargs):
+    if created:
+        UserPrivacy.objects.create(user=instance)
