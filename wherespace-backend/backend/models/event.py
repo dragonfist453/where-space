95bb3ff41be1c10042e867212e65be6d2bef98e8
Heyuan Zeng <hi@universe.observer>
2024-03-09 17:50:14 +0000

    fix: event model

--- wherespace-backend/backend/models/event.py
+++ wherespace-backend/backend/models/event.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 from .mixins import UUIDMixin
-from .space import Space
+from .space import Booking
 
 
 class Event(UUIDMixin, models.Model):
-    space = models.ForeignKey(
-        Space, on_delete=models.CASCADE, related_name="events", default=None, null=True
+    name = models.CharField(max_length=1024)
+    booking = models.ForeignKey(
+        Booking,
+        on_delete=models.CASCADE,
+        related_name="events",
+        default=None,
+        null=True,
     )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    max_attendees = models.PositiveIntegerField()
