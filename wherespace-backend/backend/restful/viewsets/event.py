103f2f63efc51bda78d093c54f5b439867ad681c
Heyuan Zeng <hi@universe.observer>
2024-03-09 23:01:58 +0000

    feat: add event filter

--- wherespace-backend/backend/restful/viewsets/event.py
+++ wherespace-backend/backend/restful/viewsets/event.py
@@ -1,3 +1,4 @@
+import django_filters
 from django.apps import apps
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status, permissions
@@ -7,6 +8,24 @@ from rest_framework.response import Response
 from ..serializers.event import EventSerializer
 
 Event = apps.get_model("backend", "Event")
+
+
+class EventFilter(django_filters.FilterSet):
+    bounding_box = django_filters.CharFilter(method="filter_bounding_box")
+
+    class Meta:
+        model = Event
+        fields = ["start_time", "end_time", "bounding_box"]
+
+    def filter_bounding_box(self, queryset, name, value: str):
+        east, north, south, west = map(float, value.split(","))
+
+        return queryset.filter(
+            booking__space__longitude__gt=west,
+            booking__space__longitude__lt=east,
+            booking__space__latitude__gt=south,
+            booking__space__latitude__lt=north,
+        )
 
 
 class EventViewSet(viewsets.ModelViewSet):
