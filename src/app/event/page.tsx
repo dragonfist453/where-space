4845693f23237047e4ab5685d35d7c130bc35e99
FriedhelmWS <352063465@qq.com>
2024-03-10 12:58:37 +0800

    fix window

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -13,16 +13,20 @@ import {
 import SendIcon from "@mui/icons-material/Send";
 import moment, { Moment } from "moment";
 import { useEffect, useState } from "react";
-import { ChatMessage, User, Event as CurrentEvent, } from "../model";
+import { ChatMessage, User, Event as CurrentEvent } from "../model";
 import ResponsiveAppBar from "@/components/AppBar";
 import axios from "axios";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 import React from "react";
+import { useSearchParams } from "next/navigation";
 
 const me = "666b32f2-4005-48cf-be54-7c3964f9978f";
 
 export default function EventPage() {
-  const id = window.location.hash.slice(1);
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  console.log(id);
 
   const [currentEvent, setCurrentEvent] = useState<CurrentEvent>({
     id: "",
@@ -63,22 +67,22 @@ export default function EventPage() {
 
   return (
     <>
-    <ResponsiveAppBar />
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="grid grid-cols-4 w-full">
-        <div className="h-screen flex flex-col  w-full">
-          <PeopleSection people={currentEvent.attendee} />
-          <div className="font-bold text-3xl h-1/2 p-8">Summary</div>
+      <ResponsiveAppBar />
+      <main className="flex flex-col items-center justify-between">
+        <div className="grid grid-cols-4 w-full">
+          <div className="flex flex-col  w-full">
+            <PeopleSection people={currentEvent.attendee} />
+            <div className="font-bold text-3xl h-1/2 p-8">Summary</div>
+          </div>
+          <Paper className="col-span-2 w-full px-12">
+            <ChatSection id={id!!} people={currentEvent.attendee} />
+          </Paper>
+          <div className="w-full">
+            <TaskSection />
+            <div className="font-bold text-3xl h-1/3 p-8">What We Do</div>
+          </div>
         </div>
-        <Paper className="col-span-2 w-full px-12">
-          <ChatSection id={id} people={currentEvent.attendee} />
-        </Paper>
-        <div className="h-screen w-full">
-          <TaskSection />
-          <div className="font-bold text-3xl h-1/3 p-8">What We Do</div>
-        </div>
-      </div>
-    </main>
+      </main>
     </>
   );
 }
@@ -158,7 +162,10 @@ function ChatSection({ id, people }: { id: string; people: User[] }) {
     setInputContent("");
   };
   return (
-    <div className="flex flex-col justify-end h-screen py-8">
+    <div
+      className="flex flex-col justify-end py-8"
+      style={{ height: "92.5vh" }}
+    >
       <div className="flex flex-col gap-2 text-xl mb-2">
         {messageList.map((message) => {
           if (message.from === "Me") {
